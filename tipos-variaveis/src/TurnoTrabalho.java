8f82ee51e169bef197e94a779ea99b1eba21f1b4
marvinOlsob <mv.olsob@gmail.com>
2022-11-18 11:49:18 -0300

    Desafio Turno de Trabalho

---
+++ tipos-variaveis/src/TurnoTrabalho.java
@@ -0,0 +1,19 @@
+public class TurnoTrabalho {
+
+
+    public static void main(String[] args) {
+        char turno = 'J';
+
+        if (turno == 'M'){
+            System.out.println("Bom Dia!");
+        } else if (turno == 'V') {
+            System.out.println("Boa Tarde!");
+        } else if (turno == 'N') {
+            System.out.println("Boa Noite!");
+        } else {
+            System.out.println("Valor Inválido!");
+        } 
+
+    }
+}
+
